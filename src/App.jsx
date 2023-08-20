57e733d021776d53db43bbc38c770a3995d6d418
Negar Baharmand <negarbaharmand@outlook.com>
2023-08-20 19:33:59 +0200

    Chat app first interface configuration

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,15 @@ import { ChatEngine } from "react-chat-engine";
 import "./App.css";
 
 const App = () => {
-  return <ChatEngine height="100vh" projectID="" userName="" userSecret="" />;
+  const projectId = import.meta.env.VITE_PROJECT_ID;
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectId}
+      userName="javascriptmastery"
+      userSecret="123123"
+    />
+  );
 };
 
 export default App;
